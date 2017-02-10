f497f7e12e4d00eeb9ff8ca7256e2d4fa0349fd1
Harrisonkamau <kamauharrison87@gmail.com>
2017-02-10 12:15:35 +0300

    plot line chart

--- js/chart.js
+++ js/chart.js
@@ -1,25 +1,81 @@
 $(document).ready(function(){
-  Highcharts.chart('container', {
-    chart: {
-      renderTo: 'container',
-      type: 'bar'
-    },
-    title: {
-      text: 'Fruit Consumption'
-    },
+  /* Initialize the chart using its constructor Highcharts.Chart
+  ** and set the options object
+  */
 
-    yAxis: {
+        // global options
+        Highcharts.setOptions({
+              chart: {
+                  backgroundColor: {
+                      linearGradient: [0, 0, 500, 500],
+                      stops: [
+                          [0, 'rgb(255, 255, 255)'],
+                          [1, 'rgb(240, 240, 255)']
+                          ]
+                  },
+                  borderWidth: 2,
+                  plotBackgroundColor: 'rgba(255, 255, 255, .9)',
+                  plotShadow: true,
+                  plotBorderWidth: 1
+              }
+          });
+
+        // first chart
+      var chartOne = new Highcharts.Chart({
+        chart: {
+          renderTo: 'container',
+          type: 'bar'
+        },
+        title: {
+          text: 'Fruit Consumption'
+        },
+        subtitle: {
+          text: "Bar graph"
+        },
+
+        yAxis: {
+          type: 'linear',
+          title: {
+            text: 'Fruit eaten'
+          }
+        },
+        series: [{
+          name: 'Jane',
+          data: [1, 0, 4]
+        },
+        {
+        name: 'John',
+        data: [5,7,3]
+      }]
+    });
+
+    // second chart
+    var chartTwo = new Highcharts.Chart({
+      chart: {
+        renderTo: 'container2',
+        type: 'line'
+      },
       title: {
-        text: 'Fruit eaten'
-      }
-    },
-    series: [{
-      name: 'Jane',
-      data: [1, 0, 4]
-    },
-    {
-    name: 'John',
-    data: [5,7,3]
-  }]
-});
+        text: "Fruit Consumption - Line graph"
+      },
+      xAxis: {
+        categories: ['Mango', 'Orange', 'Apple']
+      },
+      yAxis: {
+        type: 'linear',
+        title: {
+          text: 'Fruit eaten'
+        }
+      },
+      series: [{
+        name: 'Joan',
+        data: [2,3,5]
+      },
+      {
+      name: 'Martin',
+      data: [1,3,5]
+    }
+    ]
+  });
+
 })
